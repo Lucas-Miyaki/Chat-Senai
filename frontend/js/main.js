c2bfad9396b9a9d7da7086bc8d44a6863ac14dc8
Lucas-Miyaki <lucasmiyaki@outlook.com>
2025-05-22 08:18:34 -0300

    Alterando Servidor

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -21,7 +21,7 @@ loginButton.addEventListener('click', async (event) => {
             signup.style.display = "none";
             chat.style.display = "flex";
         
-            websocket = new WebSocket( "ws://localhost:8080");
+            websocket = new WebSocket( "wss://chat-backend-gh82.onrender.com");
             websocket.onmessage = processMessage;      
     } else {
         loginError.textContent = "Erro: " + result.message;
